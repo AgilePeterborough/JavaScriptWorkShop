c02ae4bf5d3ae55ffb3a535227a4ea34e2492505
boyney123 <davidboyne123@hotmail.co.uk>
2014-04-11 12:09:46 +0100

    Added new files and also updated the dashboard

--- Framework_Barebones/js/lib/sb-admin.js
+++ Framework_Barebones/js/lib/sb-admin.js
@@ -1,18 +1,19 @@
 $(function() {
 
-    $('#side-menu').metisMenu();
+	$('#side-menu').metisMenu();
 
 });
 
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
 $(function() {
-    $(window).bind("load resize", function() {
-        width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
-        if (width < 768) {
-            $('div.sidebar-collapse').addClass('collapse')
-        } else {
-            $('div.sidebar-collapse').removeClass('collapse')
-        }
-    })
+	Morris.Donut({
+		element: 'visitors-today',
+		data: [
+		{label: "Peterborough Visitors", value: 3000},
+		{label: "London Visitors", value: 9000},
+		{label: "Leeds Visitors", value: 2110}
+		]
+	});
+
 })
